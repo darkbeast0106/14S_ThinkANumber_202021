723f050d04131f42f0310a13e26fbf81e4cbd199
Merényi Miklós <merenyi.miklos5@gmail.com>
2021-10-06 18:06:12 +0200

    felugró ablakok hozzáadva

--- app/src/main/java/com/darkbeast0106/thinkanumber/MainActivity.java
+++ app/src/main/java/com/darkbeast0106/thinkanumber/MainActivity.java
@@ -1,12 +1,18 @@
 package com.darkbeast0106.thinkanumber;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Random;
 
@@ -17,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView felhasznaloTippText;
     private int felhasznaloTipp, gondoltSzam, maximum, elet;
     private Random random;
+    private Toast customToast;
+    private AlertDialog.Builder alertBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +38,8 @@ public class MainActivity extends AppCompatActivity {
                 felhasznaloTipp++;
                 felhasznaloTippText.setText(String.valueOf(felhasznaloTipp));
             } else {
-                // TODO: Hibaüzenet tipp nem lehet maxnál nagyobb
+                //Hibaüzenet tipp nem lehet maxnál nagyobb
+                Toast.makeText(getApplicationContext(), "Nem lehet nagyobb 10-nél!", Toast.LENGTH_SHORT).show();
             }
         });
         btnMinusz.setOnClickListener(view -> {
@@ -38,28 +47,45 @@ public class MainActivity extends AppCompatActivity {
                 felhasznaloTipp--;
                 felhasznaloTippText.setText(String.valueOf(felhasznaloTipp));
             } else {
-                // TODO: Hibaüzenet tipp nem lehet kisebb mint 1
+                //Hibaüzenet tipp nem lehet kisebb mint 1
+                /*Toast toast = new Toast(getApplicationContext());
+                toast.setText("Nem lehet kisebb 1-nél");
+                toast.setDuration(Toast.LENGTH_SHORT);
+                toast.show();*/
+                Toast.makeText(getApplicationContext(), "Nem lehet kisebb 1-nél!", Toast.LENGTH_SHORT).show();
             }
         });
         btnTipp.setOnClickListener(view -> {
-            if (felhasznaloTipp == gondoltSzam){
-                // TODO: Győzelem
+            if (felhasznaloTipp == gondoltSzam) {
+                // Győzelem ALERT DIALOG
+                //alertBuilder.setTitle("Győztél, játék vége!").create().show();
+                alertBuilder.setTitle("Játék vége");
+                alertBuilder.create();
+                alertBuilder.show();
             } else if (felhasznaloTipp < gondoltSzam) {
-                // TODO: Nagyobb számra gondoltam
+                // Nagyobb számra gondoltam EGYEDI TOAST
                 eletLevon();
+                customToast.show();
+                Toast.makeText(getApplicationContext(), "Nagyobb száma gondoltam!", Toast.LENGTH_SHORT).show();
             } else {
-                // TODO: Kisebb számra gondoltam
+                // Kisebb számra gondoltam EGYEDI TOAST
                 eletLevon();
+                customToast.show();
+                Toast.makeText(getApplicationContext(), "Kisebb száma gondoltam!", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     private void eletLevon() {
-        if (elet > 0){
+        if (elet > 0) {
             elet--;
         }
         eletek[elet].setImageResource(R.drawable.heart1);
-        if (elet == 0){
+        if (elet == 0) {
+            alertBuilder.setTitle("Játék vége");
+            alertBuilder.create();
+            alertBuilder.show();
+            //alertBuilder.setTitle("Vesztettél, játék vége!").create().show();
             // TODO: jatek vege
         }
     }
@@ -67,15 +93,15 @@ public class MainActivity extends AppCompatActivity {
     private void ujJatek() {
         felhasznaloTipp = 1;
         felhasznaloTippText.setText("1");
-        gondoltSzam = random.nextInt(maximum)+1;
+        gondoltSzam = random.nextInt(maximum) + 1;
         Log.d("gondolt szam", String.valueOf(gondoltSzam));
         elet = 4;
-        for (ImageView elet: eletek) {
+        for (ImageView elet : eletek) {
             elet.setImageResource(R.drawable.heart2);
         }
     }
 
-    private void init(){
+    private void init() {
         btnPlusz = findViewById(R.id.btn_plusz);
         btnMinusz = findViewById(R.id.btn_minusz);
         btnTipp = findViewById(R.id.btn_tipp);
@@ -86,6 +112,37 @@ public class MainActivity extends AppCompatActivity {
         felhasznaloTippText = findViewById(R.id.felhasznalo_valasztas);
         maximum = 10;
         random = new Random();
-        eletek = new ImageView[]{hp1,hp2,hp3,hp4};
+        eletek = new ImageView[]{hp1, hp2, hp3, hp4};
+        customToast = new Toast(getApplicationContext());
+        CreateCustomToast();
+        alertBuilder = new AlertDialog.Builder(getApplicationContext());
+        CreateAlertDialog();
+    }
+
+    private void CreateAlertDialog() {
+        alertBuilder.setMessage("Szeretnél e új játékot?");
+        alertBuilder.setNegativeButton("Nem", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+        alertBuilder.setPositiveButton("Igen", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                ujJatek();
+                closeContextMenu();
+            }
+        });
+        //kötelező választani
+        alertBuilder.setCancelable(false);
+    }
+
+    private void CreateCustomToast() {
+        customToast.setDuration(Toast.LENGTH_SHORT);
+        View view = getLayoutInflater().inflate(R.layout.custom_toast,
+                findViewById(R.id.custom_toast));
+        customToast.setView(view);
+        customToast.setGravity(Gravity.CENTER, 0, 0);
     }
 }
